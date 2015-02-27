398a76e0fbda73d5c9a2d4fc172bcbe46b314249
kangarie <mail@jualvpn.tk>
2015-02-27 10:15:59 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+// cpanel login
+$username = 'your-username';
+$password = 'your-password';
+
+$baseURL  = 'http://your-whmURL-without-Port.com';
+$whmPort    = 2086;
+$cPanelPort = 2082;
+
+// backup scp login
+$backupHost = 'your-host';
+$backupUser = 'your-username';
+$backupPass = 'your-password';
+$backupPort = '22';
+$backupPath = '/home/backup';
+$backupMail = 'your-email';
